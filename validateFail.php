f604ab6f87fee878d9945d9c572344c1d962ea1b
Jean <jcmend97@gmail.com>
2020-05-17 14:22:37 +1000

    added payment.html, validate.php and validateFail.php

---
+++ validateFail.php
@@ -0,0 +1,8 @@
+<?php
+//Page is currently just for testing inputs and see if they work
+//The code below will output the session errMsg.
+  session_start();
+
+  print_r($_SESSION["errMsg"]);
+  
+?>

ee2ff31803e0116bc8ba6238cc66d6ca609e22cc
NgocHuynh <huynhtransongngoc@gmail.com>
2020-05-21 22:17:24 +1000

    adding a cart-confirm

---
+++ scripts/cart_confirm.js
@@ -0,0 +1,42 @@
+function cartConfirm() {
+    const Name = document.querySelector(".product-name");
+    const Size = document.querySelector(".product-size");
+    const Price = document.querySelector(".product-price");
+    const Quantity = document.querySelector(".product-quantity");
+    const total = document.querySelector(".product-total");
+    Name.textContent = "something";
+    Size.textContent = "something";
+    Price.textContent = 10;
+    Price.value = 10;
+    Quantity.value = 2;
+    total.textContent = calcTotal();
+
+}
+
+function checkQuantity() {
+    // quantities =  sessionStorage.Quantity;
+    const confirm = document.querySelector("#cart-confirm");
+    quantities = 5;
+    for (let index = 0; index < quantities; index++) {
+        confirm.innerHTML += `<tr>
+        <td><span class="product-name">1</span></td>
+        <td><span class="product-size">1</span></td> 
+        <td><span class="product-price">1</span></td>
+        <td><span class="product-quantity">1</span></td> 
+        <td><span class="product-total">1</span></td> 
+    </tr>`;
+
+    }
+}
+
+function calcTotal(price, quantities) {
+    let total = document.querySelector(".product-total");
+    total = price * quantities;
+    return total;
+}
+
+function init() {
+    checkQuantity();
+    cartConfirm();
+}
+window.onload = init;

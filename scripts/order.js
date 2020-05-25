6886394aae77b2a24771519c082c9335c6c590cb
NgocHuynh <huynhtransongngoc@gmail.com>
2020-05-26 04:12:54 +1000

    Adding navbar  <!-- FOOTER  -->     <footer>         <p>Swin Food, Copyright@2020</p>     </footer>     <!-- FOOTER  -->     <script src="../Shared-food-ordering/scripts/nav.js"></script>

--- scripts/order.js
+++ scripts/order.js
@@ -55,35 +55,35 @@ function validate() {
     const phone = document.querySelector("#input-Phone").value;
     const email = document.querySelector("#input-Email").value;
     const order = document.querySelector("#input-Order").value;
-    const entree = document.querySelector("#input-Entree").value;
     const food = document.querySelector("#input-Food").value;
     const size = document.querySelector("#input-Size").value;
     const quantity = document.querySelector("#input-Quantity").value;
     const special = document.querySelector("#input-Special").value;
+    let price;
     // Order form 2 
-    const entree2 = document.querySelector("#input-Entree2").value;
     const food2 = document.querySelector("#input-Food-2").value;
     const size2 = document.querySelector("#input-Size-2").value;
     const quantity2 = document.querySelector("#input-Quantity-2").value;
     const special2 = document.querySelector("#input-Special-2").value;
+    let price2;
     //Order form 3
-    const entree3 = document.querySelector("#input-Entree3").value;
     const food3 = document.querySelector("#input-Food-3").value;
     const size3 = document.querySelector("#input-Size-3").value;
     const quantity3 = document.querySelector("#input-Quantity-3").value;
     const special3 = document.querySelector("#input-Special-3").value;
+    let price3;
     //Order form 4
-    const entree4 = document.querySelector("#input-Entree4").value;
     const food4 = document.querySelector("#input-Food-4").value;
     const size4 = document.querySelector("#input-Size-4").value;
     const quantity4 = document.querySelector("#input-Quantity-4").value;
     const special4 = document.querySelector("#input-Special-4").value;
+    let price4;
     //Order form 5
-    const entree5 = document.querySelector("#input-Entree5").value;
     const food5 = document.querySelector("#input-Food-5").value;
     const size5 = document.querySelector("#input-Size-5").value;
     const quantity5 = document.querySelector("#input-Quantity-5").value;
     const special5 = document.querySelector("#input-Special-5").value;
+    let price5;
 
 
 
@@ -209,7 +209,7 @@ function validate() {
     }
 
     // Phone Validate
-    if (!phone.match(/\d{3}\d{3}\d{4}/)) {
+    if (phone == "") {
         const msg = document.querySelector(".msg8");
         msg.classList.add("text-light", "bg-warning", "my-2");
         msg.innerHTML = `Please match the phone format\n`
@@ -217,18 +217,42 @@ function validate() {
         setTimeout(() => msg.remove(), 5000);
     }
 
-    //  Entree Validate
-    if (entree == "") {
-        const msg = document.querySelector(".msg9");
-        msg.classList.add("text-light", "bg-warning", "my-2");
-        msg.innerHTML = ` Please choose an entree in meal 1`
-        result = false;
-        setTimeout(() => msg.remove(), 5000);
-    }
 
     //  Food Validate
     if (food == "") {
-        const msg = document.querySelector(".msg10");
+        const msg = document.querySelector(".msg9");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please choose your meal in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+    // Food 2 Validate 
+    if (food2 == "") {
+        const msg = document.querySelector(".msg12");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please choose your meal in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+    // Food 3 Validate 
+    if (food3 == "") {
+        const msg = document.querySelector(".msg15");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please choose your meal in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+    // Food 4 Validate 
+    if (food4 == "") {
+        const msg = document.querySelector(".msg18");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please choose your meal in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+    // Food 5 Validate 
+    if (food5 == "") {
+        const msg = document.querySelector(".msg21");
         msg.classList.add("text-light", "bg-warning", "my-2");
         msg.innerHTML = ` Please choose your meal in meal 1`
         result = false;
@@ -237,7 +261,43 @@ function validate() {
 
     //  Size Validate
     if (size == "") {
-        const msg = document.querySelector(".msg11");
+        const msg = document.querySelector(".msg10");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please specify a size in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Size2 Validate
+    if (size2 == "") {
+        const msg = document.querySelector(".msg13");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please specify a size in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Size3 Validate
+    if (size3 == "") {
+        const msg = document.querySelector(".msg16");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please specify a size in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Size4 Validate
+    if (size4 == "") {
+        const msg = document.querySelector(".msg19");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` Please specify a size in meal 1`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Size5 Validate
+    if (size5 == "") {
+        const msg = document.querySelector(".msg22");
         msg.classList.add("text-light", "bg-warning", "my-2");
         msg.innerHTML = ` Please specify a size in meal 1`
         result = false;
@@ -246,54 +306,159 @@ function validate() {
 
     //  Quantity Validate
     if (quantity == 0) {
-        const msg = document.querySelector(".msg12");
+        const msg = document.querySelector(".msg11");
         msg.classList.add("text-light", "bg-warning", "my-2");
         msg.innerHTML = ` How many order do you want`
         result = false;
         setTimeout(() => msg.remove(), 5000);
     }
 
+    //  Quantity2 Validate
+    if (quantity2 == 0) {
+        const msg = document.querySelector(".msg14");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` How many order do you want`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Quantity3 Validate
+    if (quantity3 == 0) {
+        const msg = document.querySelector(".msg17");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` How many order do you want`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Quantity4 Validate
+    if (quantity4 == 0) {
+        const msg = document.querySelector(".msg20");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` How many order do you want`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    //  Quantity5 Validate
+    if (quantity5 == 0) {
+        const msg = document.querySelector(".msg23");
+        msg.classList.add("text-light", "bg-warning", "my-2");
+        msg.innerHTML = ` How many order do you want`
+        result = false;
+        setTimeout(() => msg.remove(), 5000);
+    }
+
+    price = calculatePrice(food, quantity, size, price);
+    price2 = calculatePrice(food2, quantity2, size2, price2)
+    price3 = calculatePrice(food3, quantity3, size3, price3)
+    price4 = calculatePrice(food4, quantity4, size4, price4)
+    price5 = calculatePrice(food5, quantity5, size5, price5)
     if (result) {
-        storeBooking(firstname, lastname, address1, address2, suburb, postcode, email, phone, state, order, entree, food, size, quantity, special);
+        alert("something");
+        storeBooking(firstname, lastname, address1, address2, suburb, postcode, email, phone, state, order, price, food, size, quantity, special, food2, size2, quantity2, special2, price2, food3, size3, quantity3, special3, price3, food4, size4, quantity4, special4, price4, food5, size5, quantity5, special5, price5);
     }
     return result;
 }
 
 //  Store Order 
-function storeBooking(firstname, lastname, address1, address2, suburb, postcode, email, phone, state, order, entree, food, size, quantity, special) {
-    sessionStorage.firstname = firstname;
-    sessionStorage.lastname = lastname;
-    sessionStorage.address1 = address1;
-    sessionStorage.address2 = address2;
-    sessionStorage.suburb = suburb;
-    sessionStorage.postcode = postcode;
-    sessionStorage.email = email;
-    sessionStorage.phone = phone;
-    sessionStorage.state = state;
-    sessionStorage.order = order;
-    sessionStorage.id = 1;
-    sessionStorage.food = food;
-    sessionStorage.entree1 = entree;
-    sessionStorage.mealSize1 = size;
-    sessionStorage.mealQuantity1 = quantity;
-    sessionStorage.special = special;
-    alert(sessionStorage.firstname);
-    alert(sessionStorage.lastname);
-    alert(sessionStorage.address1);
-    alert(sessionStorage.address2);
-    alert(sessionStorage.suburb);
-    alert(sessionStorage.postcode);
-    alert(sessionStorage.email);
-    alert(sessionStorage.phone);
-    alert(sessionStorage.state);
-    alert(sessionStorage.order);
-    alert(sessionStorage.id);
-    alert(sessionStorage.food);
-    alert(sessionStorage.entree1);
-    alert(sessionStorage.mealSize1);
-    alert(sessionStorage.mealQuantity1);
-    alert(sessionStorage.special);
+function storeBooking(firstname, lastname, address1, address2, suburb, postcode, email, phone, state, order, price, food, size, quantity, special, food2, size2, quantity2, special2, price2, food3, size3, quantity3, special3, price3, food4, size4, quantity4, special4, price4, food5, size5, quantity5, special5, price5) {
+    localStorage.firstname = firstname;
+    localStorage.lastname = lastname;
+    localStorage.address1 = address1;
+    localStorage.address2 = address2;
+    localStorage.suburb = suburb;
+    localStorage.postcode = postcode;
+    localStorage.email = email;
+    localStorage.phone = phone;
+    localStorage.state = state;
+    localStorage.order = order;
+    // Store order 1
+    localStorage.mealID1 = 1;
+    localStorage.food = food;
+    localStorage.mealPrice1 = price;
+    localStorage.mealSize1 = size;
+    localStorage.mealQuantity1 = quantity;
+    localStorage.special = special;
+    // Store order 2
+    localStorage.mealID2 = 2;
+    localStorage.food2 = food2;
+    localStorage.mealPrice2 = price2;
+    localStorage.mealSize2 = size2;
+    localStorage.mealQuantity2 = quantity2;
+    localStorage.special2 = special2;
+    // Store order 3
+    localStorage.mealID3 = 3;
+    localStorage.food3 = food3;
+    localStorage.mealPrice3 = price3;
+    localStorage.mealSize3 = size3;
+    localStorage.mealQuantity3 = quantity3;
+    localStorage.special3 = special3;
+    // Store order 4
+    localStorage.mealID4 = 4;
+    localStorage.food4 = food4;
+    localStorage.mealPrice4 = price4;
+    localStorage.mealSize4 = size4;
+    localStorage.mealQuantity4 = quantity4;
+    localStorage.special4 = special4;
+    // Store order 5
+    localStorage.mealID5 = 5;
+    localStorage.food5 = food5;
+    localStorage.mealPrice5 = price5;
+    localStorage.mealSize5 = size5;
+    localStorage.mealQuantity5 = quantity5;
+    localStorage.special5 = special5;
 }
+
+function calculatePrice(food, quantity, size, price) {
+    switch (food) {
+        case "pizza":
+            if (size == "large") {
+                price = (15 * quantity) * 2
+            } else if (size == "medium") {
+                price = (15 * quantity) * 1.5
+            } else if (size == "small") {
+                price = (15 * quantity)
+            }
+            break;
+        case "spaghetti":
+            if (size == "large") {
+                price = (20 * quantity) * 2
+            } else if (size == "medium") {
+                price = (20 * quantity) * 1.5
+            } else if (size == "small") {
+                price = (20 * quantity)
+            }
+            case "pasta":
+                if (size == "large") {
+                    price = (8.5 * quantity) * 2
+                } else if (size == "medium") {
+                    price = (8.5 * quantity) * 1.5
+                } else if (size == "small") {
+                    price = (8.5 * quantity)
+                }
+                case "kebab":
+                    if (size == "large") {
+                        price = (12.5 * quantity) * 2
+                    } else if (size == "medium") {
+                        price = (12.5 * quantity) * 1.5
+                    } else if (size == "small") {
+                        price = (12.5 * quantity)
+                    }
+                    case "Rolls":
+                        if (size == "large") {
+                            price = (5.50 * quantity) * 2
+                        } else if (size == "medium") {
+                            price = (5.50 * quantity) * 1.5
+                        } else if (size == "small") {
+                            price = (5.50 * quantity)
+                        }
+                        default:
+                            break;
+    }
+    return price;
+}
+
 
 function init() {
     btn1.onclick = display1;

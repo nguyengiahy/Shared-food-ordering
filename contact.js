3c122c8dfc3bde05b55447de4bf1a1169f6dfe81
Hy Nguyen <nguyengiahy2911@gmail.com>
2020-05-26 22:08:26 +1000

    fixing everything

--- contact.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.accordian h3').click(function(){
-		$(this).next('.content').slideToggle();
-		$(this).parent().toggleClass('active');
-		$(this).parent().siblings().children('.content').slideUp();
-		$(this).parent().siblings().removeClass('active');
-	});
-});
